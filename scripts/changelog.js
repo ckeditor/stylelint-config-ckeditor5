5bca5ded8c21c70c78965c8d730a8a809ac1b55d
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-09-10 13:45:40 +0200

    Internal: Clean a changelog script.

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -13,7 +13,4 @@
  * Scripts for generating the changelog before starting the release process.
  */
 
-require( '@ckeditor/ckeditor5-dev-env' ).generateChangelogForSinglePackage({
-	// TODO: Remove this line after the next release.
-	from: 'a71c869d0f46a5527c99cd21cf67f016ac431028'
-});
+require( '@ckeditor/ckeditor5-dev-env' ).generateChangelogForSinglePackage();
